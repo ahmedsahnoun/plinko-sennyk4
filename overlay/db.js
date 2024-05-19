03d463f87f732f96b6d8a8a66f09849a2ea240f2
ZiedYT <65912881+ZiedYT@users.noreply.github.com>
2024-05-19 09:57:45 +0200

    buh

--- overlay/db.js
+++ overlay/db.js
@@ -73,7 +73,7 @@ onValue(ref(database, "Manual"), (snap) => {
 	}
 })
 
-export { addSubmission, removeSubmission, updateSettings, updateTwitchToken }
+// export { addSubmission, removeSubmission, updateSettings, updateTwitchToken }
 
 
 
@@ -205,3 +205,24 @@ function preload() {
 	World.remove(world, particles[i].particle.body);
 	particlesCounter--
   }
+
+
+  setTimeout(() => document.getElementsByTagName('body')[0].style.setProperty("visibility", "hidden")
+	, 5000)
+
+const isElementLoaded = async selector => {
+	while (document.querySelector(selector) === null) {
+		await new Promise(resolve => requestAnimationFrame(resolve))
+	}
+	return document.querySelector(selector);
+};
+
+isElementLoaded("canvas").then(() => {
+	var textOption = ""
+	for (let option in options) {
+		var colorIndex = option < 6 ? option : 11 - option
+		textOption += /*html*/`<div style="background-color:${colorGradiant[colorIndex]};" >${Number(option) + 1}</div>`
+	}
+	optionsText.innerHTML = textOption
+	reconstruct()
+})
--- overlay/page.js
+++ overlay/page.js
@@ -2,22 +2,3 @@ queueLauncher = null
 idleLauncher = null
 
 
-setTimeout(() => document.getElementsByTagName('body')[0].style.setProperty("visibility", "hidden")
-	, 5000)
-
-const isElementLoaded = async selector => {
-	while (document.querySelector(selector) === null) {
-		await new Promise(resolve => requestAnimationFrame(resolve))
-	}
-	return document.querySelector(selector);
-};
-
-isElementLoaded("canvas").then(() => {
-	var textOption = ""
-	for (let option in options) {
-		var colorIndex = option < 6 ? option : 11 - option
-		textOption += /*html*/`<div style="background-color:${colorGradiant[colorIndex]};" >${Number(option) + 1}</div>`
-	}
-	optionsText.innerHTML = textOption
-	reconstruct()
-})
